5da969e1cfa219d40f6fec4311c78c7c6d501803
Ech08 <esmi134@aucklanduni.ac.nz>
2025-03-18 23:51:56 +1300

    modified createOperator method to print name, abbreviated name, and full location of new operator

--- src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
+++ src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
@@ -1,17 +1,39 @@
 package nz.ac.auckland.se281;
 
+import nz.ac.auckland.se281.Types.Location;
+
 public class OperatorManagementSystem {
 
   // Do not change the parameters of the constructor
   public OperatorManagementSystem() {}
 
   public void searchOperators(String keyword) {
-
     MessageCli.OPERATORS_FOUND.printMessage("are", "no", "s", ".");
   }
 
   public void createOperator(String operatorName, String location) {
-    // TODO implement
+    // setting up variables
+    String words[] = operatorName.split(" ");
+    String abbrevName = "";
+    boolean start = true;
+
+    // add the first letter of each word in words to abbrevName
+    for (int i = 0; i < words.length; i++) {
+      // if whitespace detected, get ready to add letter
+      if (words[i] == " ") {
+        start = true;
+      }
+      // if ready and whitespace not detected, add letter
+      else if (start = true && words[i] != " ") {
+        abbrevName = abbrevName + words[i].charAt(0);
+        start = false;
+      }
+    }
+
+    String fullLoc = Location.fromString(location).getFullName();
+
+    // print message with all operator properties
+    MessageCli.OPERATOR_CREATED.printMessage(operatorName, abbrevName, fullLoc);
   }
 
   public void viewActivities(String operatorId) {
