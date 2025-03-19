de3ed84f4cd64b4f8e66964a016154ec9a79cf6e
Ech08 <esmi134@aucklanduni.ac.nz>
2025-03-19 14:14:23 +1300

    added idNum method to increment a 3 digit number by 1, added comments to createOperator

--- src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
+++ src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
@@ -4,6 +4,38 @@ import nz.ac.auckland.se281.Types.Location;
 
 public class OperatorManagementSystem {
 
+  // custom methods:
+  public String idNum(int num) {
+    // increments input int by 1 and returns 3 digit string
+    int digits[] = {0, 0, 0};
+    boolean carryOut = true;
+    for (int i = 2; i < digits.length; i--) {
+      if (carryOut) {
+        switch (digits[i]) {
+          case 9:
+            if (i == 0) {
+              // set to 999 (largest possible value)
+              digits[0] = 9;
+              digits[1] = 9;
+              digits[2] = 9;
+              carryOut = false;
+            } else {
+              digits[i] = 0;
+              carryOut = true;
+            }
+            break;
+
+          default:
+            // add one to digit
+            digits[i]++;
+            carryOut = false;
+            break;
+        }
+      }
+    }
+    return digits.toString();
+  }
+
   // Do not change the parameters of the constructor
   public OperatorManagementSystem() {}
 
@@ -12,6 +44,7 @@ public class OperatorManagementSystem {
   }
 
   public void createOperator(String operatorName, String location) {
+    // get operatorName abbreviation
     String words[] = operatorName.split(" ");
     String abbrevName = "";
 
@@ -20,6 +53,7 @@ public class OperatorManagementSystem {
       abbrevName = abbrevName + word.charAt(0);
     }
 
+    // get full location name
     String fullLoc = Location.fromString(location).getFullName();
 
     // print message with all operator properties
